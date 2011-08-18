a485b3a213e89068d2988014431d68ecc116b4e9
Manuel Francisco Naranjo <manuel@aircable.net>
2011-08-17 22:28:21 -0300

    Added vmware build script

---
+++ build-vmware.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+TARGET="ubuntu-$(date +%s)"
+CWD=$(pwd)
+
+sudo vmbuilder vmserver ubuntu \
+    -c vmbuilder.cfg \
+    --templates=templates \
+    --copy $CWD/copy-files \
+    --exec $CWD/post-install.sh \
+    --firstboot $CWD/first-boot.sh \
+    --mem 256 \
+    -v --debug -d ${TARGET} 2>&1 | tee build.log
+
+sudo chown -R $(whoami):$(whoami) $TARGET
